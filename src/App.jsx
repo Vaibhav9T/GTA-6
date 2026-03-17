316e856dc1cad5bf2c21390696be8be168127ee3
Vaibhav Annaso Tembukade <vaibhavtembukadea09@gmail.com>
2026-03-17 13:59:15 +0530

    feat : added bottombar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {useGSAP} from '@gsap/react'
 import gsap from 'gsap'
 import { useState } from 'react'
+import 'remixicon/fonts/remixicon.css'
 
 function App() {
 
@@ -68,16 +69,28 @@ function App() {
       <div className='main w-full '>
         <div className='landing w-full h-screen bg-black'>
           <div className="navbar absolute top-0 left-0 z-[10] w-full py-10 px-1">
-            <div className='logo'>
-              <div className="lines"></div>
-              <h3 className='text-3xl bold text-white'>Rockstar</h3>
+            <div className='logo flex gap-7'>
+              <div className="lines flex flex-col gap-[5px]">
+              <div className="line w-15 h-2 bg-white"></div>
+              <div className="line w-8 h-2 bg-white"></div>
+              <div className="line w-5 h-2 bg-white"></div>
+            </div>
+              <h3 className='text-4xl mt-[-9px] leading-none bold text-white'>Rockstar</h3>
             </div>
           </div>
-            <div className='relative overflow-hidden imageDiv w-full h-screen'>
-              <img className="absolute top-0 left-0 w-full h-full object-cover" src="./sky.png" alt="" />
-              <img className="absolute top-0 left-0 w-full h-full object-cover" src="./bg.png" alt="" />
-              <img className="absolute bottm-[25%] left-1/2 -translate-x-1/2 scale[1.4]" src="./girlbg.png" alt="" />
+          <div className='relative overflow-hidden imageDiv w-full h-screen'>
+            <img className="absolute top-0 left-0 w-full h-full object-cover" src="./sky.png" alt="" />
+            <img className="absolute top-0 left-0 w-full h-full object-cover" src="./bg.png" alt="" />
+            <img className="absolute bottm-[25%] left-1/2 -translate-x-1/2 scale[1.4]" src="./girlbg.png" alt="" />
+          </div>
+          <div className="btmbar text-white absolute bottom-0 left-0 w-full py-10 px-10 bg-gradient-to-t from-black to-transparent">
+            <div className=' items-center flex gap-4'>
+              <i class="text-4xl ri-arrow-down-long-line"></i>
+              <h3 className="text-xxl font-[Helvitica_Now_Display]">Scroll Down</h3>
             </div>
+            <img className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[65px]" src="./ps5.png" alt="" />
+          </div>
+          
         </div>
       </div>
       }
